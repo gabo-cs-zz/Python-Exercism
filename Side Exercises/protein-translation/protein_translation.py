ead86c808cd8fd407c6980ad6a44e2bb087c1658
Gabriel Coronado <gabrielomar2809@gmail.com>
2020-03-13 22:11:03 -0500

    Protein Translation

---
+++ Side Exercises/protein-translation/protein_translation.py
@@ -0,0 +1,32 @@
+PROTEINS = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP',
+    'UAG': 'STOP',
+    'UGA': 'STOP'
+}
+
+
+def proteins(strand):
+    proteins = []
+    for codon in codons(strand):
+        if PROTEINS[codon] == 'STOP':
+            break
+        proteins.append(PROTEINS[codon])
+    return proteins
+
+
+def codons(strand):
+    return (strand[i:3+i] for i in range(0, len(strand), 3))
